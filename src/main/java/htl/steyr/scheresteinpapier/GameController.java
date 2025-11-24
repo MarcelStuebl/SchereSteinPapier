c2a12090c344b2a177a431e0bbe956d49550e29e
Marcel <123411996+MarcelStuebl@users.noreply.github.com>
2025-11-24 13:58:44 +0100

    grundlegende game-view.fxml, dateinamen angeändert

---
+++ src/main/java/htl/steyr/scheresteinpapier/GameController.java
@@ -0,0 +1,36 @@
+package htl.steyr.scheresteinpapier;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class GameController {
+    public Button reviewButton;
+    public Button schereLeftButton;
+    public Button steinLeftButton1;
+    public Button papierLeftButton1;
+    public Button schereRightButton1;
+    public Button steinRightButton11;
+    public Button papierRightButton11;
+
+
+    public void reviwOnButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void schereLeftOnButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void steinLeftOnButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void papierLeftOnButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void schereRightOnButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void steinRightOnButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void papierRightOnButtonPressed(ActionEvent actionEvent) {
+    }
+}
