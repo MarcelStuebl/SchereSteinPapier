c2e4e42bbcc6bff5c4b8d26f35a1d7319c30fb08
Marcel <marcel@stuebl.eu>
2025-12-01 14:15:49 +0100

    Add header comments and project description to Launcher.java

--- src/main/java/htl/steyr/scheresteinpapier/Launcher.java
+++ src/main/java/htl/steyr/scheresteinpapier/Launcher.java
@@ -1,3 +1,19 @@
+/*-----------------------------------------------------------------------------
+ *              Hoehere Technische Bundeslehranstalt STEYR
+ *                  Fachrichtung Informationstechnologie
+ *----------------------------------------------------------------------------*/
+/**
+ * Kurzbeschreibung
+ *
+ * @author  : Marcel STÜBL, Moritz RASCHKO
+ * @date    : 01.12.2025
+ *
+ * @details
+ *   Rock Paper Scissors (Schere Stein Papier) is a desktop application that brings the classic hand game to your screen with a modern JavaFX interface.
+ *   This project extends the traditional game with the Well (Brunnen) option, adding an extra layer of strategy!
+ *
+ */
+
 package htl.steyr.scheresteinpapier;
 
 import javafx.application.Application;
@@ -7,3 +23,7 @@ public class Launcher {
         Application.launch(GameApplication.class, args);
     }
 }
+
+
+
+
