be036c512d0f26a5f0f2efc00942c98a65d5d9b2
Marcel <marcel@stuebl.eu>
2025-12-04 12:24:06 +0100

    Add highscore management to User and DatabaseUser classes

--- src/main/java/htl/steyr/scheresteinpapier/Model/User.java
+++ src/main/java/htl/steyr/scheresteinpapier/Model/User.java
@@ -3,31 +3,46 @@ package htl.steyr.scheresteinpapier.Model;
 import javafx.beans.property.*;
 
 public class User {
-    private final IntegerProperty id;
-    private final StringProperty vorname;
-    private final StringProperty nachname;
+    private int id;
+    private String vorname;
+    private String nachname;
+    private int highscore;
 
     public User() {
-        this. id = new SimpleIntegerProperty();
-        this.vorname = new SimpleStringProperty();
-        this.nachname = new SimpleStringProperty();
+        this.id = 0;
+        this.vorname = "";
+        this.nachname = "";
+        this.highscore = 0;
     }
 
-    public User(int id, String vorname, String nachname) {
-        this.id = new SimpleIntegerProperty(id);
-        this.vorname = new SimpleStringProperty(vorname);
-        this.nachname = new SimpleStringProperty(nachname);
+    public User(int id, String vorname, String nachname, int highscore) {
+        this.id = id;
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.highscore = highscore;
     }
 
     // ID
-    public int getId() { return id.get(); }
-    public void setId(int id) { this. id.set(id); }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     // Vorname
-    public String getVorname() { return vorname.get(); }
-    public void setVorname(String vorname) { this.vorname. set(vorname); }
+    public String getVorname() { return vorname; }
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
 
     // Nachname
-    public String getNachname() { return nachname.get(); }
-    public void setNachname(String nachname) { this. nachname.set(nachname); }
+    public String getNachname() { return nachname; }
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+
+    public int getHighscore() {
+        return highscore;
+    }
+    public void setHighscore(int highscore) {
+        this.highscore = highscore;
+    }
 }
--- src/main/java/htl/steyr/scheresteinpapier/database/DatabaseUser.java
+++ src/main/java/htl/steyr/scheresteinpapier/database/DatabaseUser.java
@@ -11,7 +11,7 @@ public class DatabaseUser {
 
     public List<User> getAllUsers() throws SQLException {
         List<User> users = new ArrayList<>();
-        String query = "SELECT id, vorname, nachname FROM user";
+        String query = "SELECT id, vorname, nachname, highscore FROM user";
 
         try (Connection conn = DatabaseConnection.getConnection();
              Statement stmt = conn.createStatement();
@@ -19,9 +19,10 @@ public class DatabaseUser {
 
             while (rs.next()) {
                 User user = new User(
-                        rs. getInt("id"),
-                        rs. getString("vorname"),
-                        rs. getString("nachname")
+                        rs.getInt("id"),
+                        rs.getString("vorname"),
+                        rs.getString("nachname"),
+                        rs.getInt("highscore")
                 );
                 users.add(user);
             }
@@ -31,13 +32,14 @@ public class DatabaseUser {
 
 
     public void addUser(User user) throws SQLException {
-        String query = "INSERT INTO user (vorname, nachname) VALUES (?, ?)";
+        String query = "INSERT INTO user (vorname, nachname, highscore) VALUES (?, ?, ?)";
 
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
 
-            pstmt. setString(1, user.getVorname());
-            pstmt.setString(2, user. getNachname());
+            pstmt.setString(1, user.getVorname());
+            pstmt.setString(2, user.getNachname());
+            pstmt.setInt(3, user.getHighscore());
             pstmt.executeUpdate();
 
             ResultSet rs = pstmt.getGeneratedKeys();
@@ -46,6 +48,21 @@ public class DatabaseUser {
             }
         }
     }
+
+
+    public void updateUserScore(User user) throws SQLException {
+        String query = "UPDATE user SET highscore = ? WHERE id = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn. prepareStatement(query)) {
+
+            pstmt.setInt(1, user.getHighscore());
+            pstmt.setInt(2, user.getId());
+            pstmt.executeUpdate();
+        }
+    }
+
+
 
 
 
